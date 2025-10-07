a048f79743ac2dda586ae67a5fa89af3b54bc134
Ankit-Nimbolkar <ankitnimbolkar272005@gmail.com>
2025-10-08 01:29:11 +0530

    send

---
+++ Ankit_FBS_WorkSpace/Core_Python/Demo_Python/swapeasy.py
@@ -0,0 +1,7 @@
+x = int(input("Enter value of x : ")) #10
+y = int(input("Enter value of y : ")) #20
+
+print(f'\nValues before swapping are x:{x} and y:{y} \n')
+
+x,y=y,x
+print(f'\nValues After swapping are x:{x} and y:{y} \n')
