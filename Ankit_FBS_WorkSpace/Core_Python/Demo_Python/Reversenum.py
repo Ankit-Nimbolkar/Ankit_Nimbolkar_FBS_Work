896263a02c5d6b02f7881be06470a4d12852db95
Ankit-Nimbolkar <ankitnimbolkar272005@gmail.com>
2025-10-12 13:59:32 +0530

    send

---
+++ Ankit_FBS_WorkSpace/Core_Python/Demo_Python/Reversenum.py
@@ -0,0 +1,8 @@
+num = 123
+S3 = 123 % 10       # S3= 3
+S1 = 123 // 100     # S1= 1
+Temp = 123 // 10    # Temp= 12
+S2 = Temp % 10      # S2= 2 
+
+print(f'Original Sequence : {num}')
+print(f'Reverse Sequence : {S3}{S2}{S1}')
--- Ankit_FBS_WorkSpace/Core_Python/Demo_Python/swapeasy.py
+++ Ankit_FBS_WorkSpace/Core_Python/Demo_Python/swapeasy.py
@@ -5,3 +5,14 @@ print(f'\nValues before swapping are x:{x} and y:{y} \n')
 
 x,y=y,x
 print(f'\nValues After swapping are x:{x} and y:{y} \n')
+
+
+#-----How Logic Work-------
+# What actually happens:
+# x, y = y, x works in two phases:
+# Right-hand side is evaluated first
+# y, x becomes (20, 10)
+# Python creates a tuple (20, 10) and stores it temporarily.
+# Left-hand side unpacking
+# x, y = (20, 10)
+# So x = 20 and y = 10.
