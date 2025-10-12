24e0fb54e2b86e32a828174abb9b828a62a33f67
Ankit-Nimbolkar <ankitnimbolkar272005@gmail.com>
2025-10-12 14:19:14 +0530

    updated code

--- Ankit_FBS_WorkSpace/Core_Python/Demo_Python/Reversenum.py
+++ Ankit_FBS_WorkSpace/Core_Python/Demo_Python/Reversenum.py
@@ -1,7 +1,8 @@
 num = 123
-S3 = 123 % 10       # S3= 3
-S1 = 123 // 100     # S1= 1
-Temp = 123 // 10    # Temp= 12
+num2 = num 
+S3 = num2 % 10       # S3= 3
+S1 = num2 // 100     # S1= 1
+Temp = num2 // 10    # Temp= 12
 S2 = Temp % 10      # S2= 2 
 
 print(f'Original Sequence : {num}')
